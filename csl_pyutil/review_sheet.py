0c9bf870beeb8af2cf2f66a9f4c66df0c346ffe8
Mārcis Gasūns <gasyoun@users.noreply.github.com>
2026-09-05 05:41:46 +0300

    chore(release): v0.24.0 (#43)

--- csl_pyutil/review_sheet.py
+++ csl_pyutil/review_sheet.py
@@ -33,7 +33,7 @@ import warnings
 
 from csl_pyutil.evidence import PreflightError, PreflightWarning, preflight
 
-__version__ = "0.23.1"
+__version__ = "0.24.0"
 
 __all__ = ["render_review_sheet", "render_review_sheet_packset", "esc", "mark_cyrillic",
            "RU_UI_STRINGS"]
